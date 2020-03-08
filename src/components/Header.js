a1882f9ff20f4a2788f9d54e73a5f199025fe977
Leonardo Matheus <lleomath@gmail.com>
2020-03-08 01:50:11 -0300

    Bulma css framework adicionado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Bulma from 'bulma'
 
 export default class components extends Component {
     render() {
@@ -14,5 +15,5 @@ export default class components extends Component {
         )
     }
 
-    
+
 }
